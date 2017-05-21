740854ebb9283af70695aba742ba71ee922f80ae
Vail Algatt <vail.algatt@gmail.com>
2017-05-21 11:12:58 -0600

    some styling changes. Not totally functional yet. Health & Hits not updating on screen when buttons are clicked.

--- script/controller.js
+++ script/controller.js
@@ -1,47 +1,75 @@
-//controller 
+// //controller 
+// function GameController() {
+//     var gameService = new GameService()
+
+//     function drawGame() {
+//         var health = gameService.getHealth()
+//         if (health <= 0) {
+//             document.getElementById("victory").innerText = "You have found the gold of El Dorado"
+//         }
+//         document.getElementById("health").innerText = health
+//     }
+
+    
+//     this.dig = function() {
+//         gameService.modifyHealth("dig")
+//         drawGame()    
+//     }
+
+//      this.sweep = function() {
+//         gameService.modifyHealth("sweep")
+//         drawGame()    
+//     }
+
+//     this.metalDetector = function() {
+//         gameService.modifyHealth("metalDetector")
+//         drawGame()    
+//     }
+
+//      this.reset = function() {
+//         gameService.modifyHealth("reset")
+//         document.getElementById("victory").innerText = ""
+//         drawGame()
+//     }
+    
+//     this.water = function(){
+//         gameService.activate("water")
+//         drawGame()
+//     }
+
+//        this.shovel = function(){
+//         gameService.activate("shovel")
+//         drawGame()
+//     }
+    
+
+//     drawGame()
+// }
+
 function GameController() {
-    var gameService = new GameService()
 
-    function drawGame() {
-        var health = gameService.getHealth()
-        if (health <= 0) {
-            document.getElementById("victory").innerText = "You have found the gold of El Dorado"
-        }
-        document.getElementById("health").innerText = health
-    }
 
-    
-    this.dig = function() {
-        gameService.modifyHealth("dig")
-        drawGame()    
-    }
+  var service = new GameService()
 
-     this.sweep = function() {
-        gameService.modifyHealth("sweep")
-        drawGame()    
-    }
+  this.onAttackClick = function attack(targetName, type) {
+    service.processAttack(targetName, type)
+    update(service.getTarget(targetName))
 
-    this.metalDetector = function() {
-        gameService.modifyHealth("metalDetector")
-        drawGame()    
-    }
+  }
 
-     this.reset = function() {
-        gameService.modifyHealth("reset")
-        document.getElementById("victory").innerText = ""
-        drawGame()
-    }
-    
-    this.water = function(){
-        gameService.activate("water")
-        drawGame()
-    }
+function update(target) {
+  if(target.name=="The player1"){
+   document.getElementById('healthBar').innerHTML = target.health;
+   document.getElementById('hits').innerHTML = target.hits  
+  }
+  else if(target.name=="Peanut"){
+    document.getElementById('healthBarTwo').innerHTML = target.health;
+   document.getElementById('hitsTwo').innerHTML = target.hits
+  } 
+ }
 
-       this.shovel = function(){
-        gameService.activate("shovel")
-        drawGame()
-    }
-    
+this.giveItem = function getItem(targetName, itemName){
+  service.processItem(targetName, itemName)
+}
 
-    drawGame()
 }
--- script/service.js
+++ script/service.js
@@ -1,65 +1,132 @@
-function GameService() {
-    var health = 100;
-    this.getHealth = function(){
-        return health     
-    }
+// function GameService() {
+//     var health = 100;
+//     this.getHealth = function(){
+//         return health     
+//     }
 
-    var actions = {
-        dig: 10,
-        sweep: 20,
-        metalDetector: 25,
-        reset: -100
-    }
+//     var actions = {
+//         dig: 10,
+//         sweep: 20,
+//         metalDetector: 25,
+//         reset: -100
+//     }
 
-    var water = {
-        modifyAmount: 10,
-        used: false,
-        active: false
-    }
+//     var water = {
+//         modifyAmount: 10,
+//         used: false,
+//         active: false
+//     }
 
-    var shovel = {
-        modifyAmount: 15,
-        used: false,
-        active: false
-    }
+//     var shovel = {
+//         modifyAmount: 15,
+//         used: false,
+//         active: false
+//     }
 
-    this.activate = function(mod){
-        if(mod == "water"){
-        water.active = true
-        } if(mod == "shovel"){
-        shovel.active = true
-        }
-    }
-    
-
-    this.modifyHealth = function(action) {
-
-        health -= actions[action]
-        
-        if(water.active == true && water.used == false){
-            health -= water.modifyAmount
-            water.used = true 
-        }
-        if(shovel.active == true && shovel.used == false && action == "dig"){
-            health -= shovel.modifyAmount
-            shovel.used = true
-        }
-
-        
-
-        if (health <= 0) {
-            health = 0
-        }
-
-        if(health > 100){
-            health = 100
-        }
-
-    }
+//     this.activate = function(mod){
+//         if(mod == "water"){
+//         water.active = true
+//         } if(mod == "shovel"){
+//         shovel.active = true
+//         }
+//     }
 
 
-}
+//     this.modifyHealth = function(action) {
 
-// this.getAction = function getAction(){
-//     return action.slice(0, action.length)
+//         health -= actions[action]
+
+//         if(water.active == true && water.used == false){
+//             health -= water.modifyAmount
+//             water.used = true 
+//         }
+//         if(shovel.active == true && shovel.used == false && action == "dig"){
+//             health -= shovel.modifyAmount
+//             shovel.used = true
+//         }
+
+
+
+//         if (health <= 0) {
+//             health = 0
+//         }
+
+//         if(health > 100){
+//             health = 100
+//         }
+
+//     }
+
+
 // }
+
+
+function GameService() {
+
+  function TargetMaker(name, health, dig, sweep, metalDetector) {
+    this.name = name
+    this.health = health
+    this.hits = 0
+    this.dig = dig
+    this.sweep = sweep
+    this.metalDetector = metalDetector
+    this.items = []
+  }
+
+  var player1 = new TargetMaker("player1", 100, 7, 5, 10)
+  var player2 = new TargetMaker("player2", 100, 9, 10, 20)
+
+  this.getTarget = function getTarget(name) {
+    if (name == player1.name) {
+      return player1
+    }
+    else if (name == player2.name) {
+      return player2
+    }
+  }
+
+
+  this.processAttack = function attack(targetName, type) {
+      var target = this.getTarget(targetName)
+      if(!target){
+        alert("ERROR")
+      }
+      
+      var totalMod = calcMods(target)
+      target.health -= target[type] * totalMod
+      target.hits ++
+    }
+
+    function calcMods(target){
+      var totalMods = 1
+      for (var i = 0; i < target.items.length; i++) {
+        var item = target.items[i];
+        totalMods -= item.modifier
+      }
+      return totalMods
+    }
+
+
+ function ItemMaker(name, modifier, description){
+ this.name = name
+ this.modifier = modifier
+ this.description = description
+ }
+
+ var items = {
+   water: new ItemMaker('water', 0.5, "Hydrate!"),
+   shovel: new ItemMaker('shovel', 0.3, 'Give your hannds a break, use a shovel!'),
+   heal: new ItemMaker('heal', 0.7, 'Take your rest day'),
+ }
+
+ this.processItem = function(targetName, itemName){
+   var target = this.getTarget(targetName)
+   if(!target){
+     alert("ERROR")
+   }
+   target.items.push(items[itemName])
+   console.log(target)
+ }
+
+
+  }
